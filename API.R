7d1b51dd8027aea55a3f1baf1cba509176b8a932
Chris Hill <cjhill@ncsu.edu>
2024-11-26 17:10:31 -0500

    blank confusion png

--- API.R
+++ API.R
@@ -1,3 +1,72 @@
-library(plumber)
 
+library(tidymodels)
+library(ggplot2)
+
+data <- readRDS("processed_data.rds")
 final_rf_model <- readRDS("final_rf_model.rds")
+
+#* Predict diabetes status
+#* @param BMI Numeric. Body Mass Index. Default: 28
+#* @param Sex Factor. Default: "Female"
+#* @param Age Factor. Default: "70-74"
+#* @param Income Factor. Default: "$10,000 to $15,000"
+#* @param HeartDiseaseorAttack Factor. Default: "No" 
+#* @post /predict
+function(BMI = 28, 
+         Sex = "Female", 
+         Age = "70-74", 
+         Income = "$10,000 to $15,000", 
+         HeartDiseaseorAttack = "No") {
+  
+  input_data <- data.frame(
+    BMI = as.numeric(BMI),
+    Sex = factor(Sex, 
+                 levels = levels(data$Sex)),
+    Age = factor(Age, 
+                 levels = levels(data$Age)),
+    Income = factor(Income, 
+                    levels = levels(data$Income)),
+    HeartDiseaseorAttack = factor(HeartDiseaseorAttack, 
+                                  levels = levels(data$HeartDiseaseorAttack))
+  )
+  
+  predict(final_rf_model, new_data = input_data, type = "prob")
+}
+
+#* Plot the confusion matrix
+#* @serializer png
+#* @get /confusion
+function() {
+  # Generate predictions
+  predictions <- predict(final_rf_model, new_data = data, type = "class")
+  
+  # Ensure predictions are added as a column
+  cm_data <- data.frame(
+    truth = factor(data$Diabetes_binary, levels = levels(data$Diabetes_binary)),
+    prediction = factor(predictions$.pred_class, levels = levels(data$Diabetes_binary))
+  )
+  
+  # Compute the confusion matrix
+  cm <- conf_mat(data = cm_data, truth = truth, estimate = prediction)
+  
+  print(cm)
+  
+  # Use autoplot to create a simple visualization
+  autoplot(cm, type = "heatmap")
+}
+
+
+
+
+#* Information about the API
+#* @get /info
+function() {
+  list(
+    name = "Chris Hill",
+    github_pages_url = "https://cjhill-ncsu.github.io/final_project/"
+  )
+}
+
+# Start the Plumber API
+# plumb("API.R")$run(port = 8000)
+
---
+++ runAPI.R
@@ -0,0 +1,10 @@
+#Run my API
+
+library(plumber)
+r <- plumb("API.R")
+
+#run it on the port in the Dockerfile
+r$run(port=8000)
+
+
+
